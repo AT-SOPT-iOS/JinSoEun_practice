fc2955e2d062ab2b438f71995af9d17df13d9601
jinsoeun <rosejinse@naver.com>
2025-04-08 00:11:59 +0900

    [Feat] LoginViewController 화면 구성

---
+++ SOPT_36_practice/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  SOPT_36_practice
+//
+//  Created by 쏘 on 4/7/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(loginButton)
+    }
+    
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 70, y: 163, width: 250, height: 44))
+        label.text = "동네라서 가능한 모든 것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = .boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    let  idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue:227/255, alpha: 1)
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue:227/255, alpha: 1)
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 422, width: 335, height: 57))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        return button
+    }()
+        
+    
+    
+}
+
+#Preview {
+    LoginViewController()
+}
