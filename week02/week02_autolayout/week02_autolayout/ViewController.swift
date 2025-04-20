9f18e242c2fced2693352f3bf9f7b710ce13e1fd
jinsoeun <rosejinse@naver.com>
2025-04-21 01:16:58 +0900

    [chore] 프로젝트 생성 및 storyboard 삭제

---
+++ week02/week02_autolayout/week02_autolayout/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  week02_autolayout
+//
+//  Created by 쏘 on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
