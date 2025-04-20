5b9f2eb767b9a6df55dd227cce02ca843ee3a91f
jinsoeun <rosejinse@naver.com>
2025-04-21 00:35:04 +0900

    [feat] closure 적용

---
+++ week02/week02/LoginViewController_Closure.swift
@@ -0,0 +1,85 @@
+//
+//  LoginViewController_Closure.swift
+//  week02
+//
+//  Created by 쏘 on 4/20/25.
+//
+
+import UIKit
+
+class LoginViewController_Closure: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(loginButton)
+    }
+    
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 70, y: 163, width: 250, height: 44))
+        label.text = "동네라서 가능한 모든 것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let  idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue:227/255, alpha: 1)
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 0))
+        textField.leftViewMode = .always
+        textField.layer.cornerRadius = 3
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue:227/255, alpha: 1)
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 0))
+        textField.leftViewMode = .always
+        textField.layer.cornerRadius = 3
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 422, width: 335, height: 57))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    private func pushToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController_Closure()
+        welcomeViewController.id = idTextField.text
+        welcomeViewController.loginDataCompletion = { [weak self] data in
+            print("클로저로 받아온 id: ", data)
+            guard let self else { return }
+            self.passwordTextField.text = data
+        }
+        
+        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
+    
+    @objc
+    private func loginButtonDidTap() {
+        pushToWelcomeVC()
+    }
+    
+        
+    
+    
+}
