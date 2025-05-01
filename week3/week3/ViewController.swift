55f488f5fbc3301c5621104379e522a6fa09b828
jinsoeun <rosejinse@naver.com>
2025-05-02 01:12:21 +0900

    [chore] 프로젝트 기본 세팅

---
+++ week3/week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3
+//
+//  Created by 쏘 on 5/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
